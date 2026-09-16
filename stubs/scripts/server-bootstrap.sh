e34d839e0b14c1617f71c40b0eff42d94aef02e6
jcergolj <me@jcergolj.me.uk>
2026-09-16 21:51:46 +0200

    Suppress generated bootstrap source warning

--- stubs/scripts/server-bootstrap.sh
+++ stubs/scripts/server-bootstrap.sh
@@ -15,7 +15,7 @@ if ! flock -n 9; then
     exit 1
 fi
 
-# shellcheck source=stubs/scripts/lib/common.sh
+# shellcheck disable=SC1091
 source "$SCRIPT_DIR/lib/common.sh"
 
 GITHUB_REPOSITORY='__GITHUB_REPOSITORY__'
